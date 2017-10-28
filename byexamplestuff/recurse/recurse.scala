51e89e713a7024a1e447f2de97e03f6f0e103848
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-28 13:19:40 -0400

    more scala fiddlign

---
+++ byexamplestuff/recurse/recurse.scala
@@ -0,0 +1,10 @@
+import math.abs
+object recurse {
+	def modNum = 21 % 19
+
+	def main(Args: Array[String]) = {
+		println("hello world. 21%19 is " + modNum + ".")
+		println("Now that that's out of the way, let's compute the gcd of 14 and 21")
+		println("another line!")
+	}
+}
