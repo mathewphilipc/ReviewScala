741e162e62f8a4dd34f838edae1e95431948a4cb
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-28 14:57:15 -0400

    more functional fiddling with sums

--- functionalByExample/basicFunctional/sums.scala
+++ functionalByExample/basicFunctional/sums.scala
@@ -1,11 +1,10 @@
 object sums {
 	def main(args: Array[String]) = {
 		println("let's do some summation")
-		println("The sum of all integers from 5 to 9 is " + sumInts(5,9))
-		println("The square of 9 is " + square(9))
-		println("The sum of squares from 1^2 to 4^2 is " + sumSquares(1,4))
-		println("The sum of powers of 2 from 2^0 to 2^4 is " + sumPowersOfTwo(0,4))
-		println("Computed more nicely, that last sum is still " + sum(powerOfTwo, 0, 4))
+		println("The sum of all integers from 1 to 5 is " + sumInts(1,5))
+		println("The sum of squares from 1 to 5 is " + sumSquares(1,5))
+		println("The sum of powers of 2 from 1 to 5 is " + sumPowersOfTwo(1,5))
+		println("Computed more nicely, that last sum is still " + sum(powerOfTwo, 1, 5))
 	}
 
 	def sumInts(a: Int, b: Int): Int = {
