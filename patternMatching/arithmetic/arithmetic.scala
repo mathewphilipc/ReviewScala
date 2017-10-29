6abc6962b795ef815d377f70654a085174715c91
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-29 17:27:33 -0400

    arithmetic!

--- patternMatching/arithmetic/arithmetic.scala
+++ patternMatching/arithmetic/arithmetic.scala
@@ -1,10 +1,12 @@
 object arithmetic {
-	def main(args: Array[String]) = println("Let's implement arithmetic expressions")
+	def main(args: Array[String]) = {
+		println("Let's implement arithmetic expressions")
+	}
 
 	abstract class Expr {
 		def isNumber: Boolean
 		def isSum: Boolean
-		def numValue: implement
+		def numValue: Int
 		def leftOp: Expr
 		def rightOp: Expr
 	}
@@ -13,15 +15,19 @@ object arithmetic {
 		def isNumber: Boolean = true
 		def isSum: Boolean = false
 		def numValue: Int = n
-		def leftOp: Expr = error("Number.leftOp")
-		def rightOp: Expr = error("Number.rightOp")
+		def leftOp: Expr = throw new Exception("Number.leftOp")
+		def rightOp: Expr = throw new Exception("Number.rightOp")
 	}
 
-	def Sum(e1: Expr, e2: Expr) extends Expr {
+	class Sum(e1: Expr, e2: Expr) extends Expr {
 		def isNumber: Boolean = false
 		def isSum: Boolean = true
-		def numValue: Int = error("Sum.numValue")
+		def numValue: Int = throw new Exception("Sum.numValue")
 		def leftOp: Expr = e1
 		def rightOp: Expr = e2
+	}
+
+	def eval(e: Expr): Int = {
+		5
 	}
 }
